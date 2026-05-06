745a093f6869c26837c0c47dea57427f80cf5946
nilFinx <nulflox@disroot.org>
2026-05-06 20:07:57 +0900

    Relicense ToxCore binding in MIT

--- Plugins/Tox/ToxCore.cs
+++ Plugins/Tox/ToxCore.cs
@@ -1,12 +1,29 @@
 ﻿/*==========================================================*/
-// Skymu is copyrighted by The Skymu Team.
-// For any inquiries or concerns, email contact@skymu.app.
+// This file is licensed in MIT.
 /*==========================================================*/
-// Modification or redistribution of this code is contingent
-// on your agreement to be bound by the terms of our License.
-// If you do not wish to abide by those terms, you may not
-// use, modify, or distribute any code from the Skymu project.
-// License: https://skymu.app/legal/license
+// Copyright 2026 The Skymu Team
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated
+// documentation files (the “Software”), to deal in the
+// Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice
+// shall be included in all copies or substantial portions of
+// the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY
+// KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+// OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 /*==========================================================*/
 
 // Also grab ToxOO.cs if you prefer object-oriented.
