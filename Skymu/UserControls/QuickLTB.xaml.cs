f416e1965527793ce889cc80ceca1905eecad667
OmegaAOL <hackersword666@gmail.com>
2026-06-18 01:45:54 -0700

    Add license info to XAML files

--- Skymu/UserControls/QuickLTB.xaml.cs
+++ Skymu/UserControls/QuickLTB.xaml.cs
@@ -1,4 +1,15 @@
-﻿using System.Collections.ObjectModel;
+﻿/*==========================================================*/
+// Skymu is copyrighted by The Skymu Team, 2026.
+// For any inquiries or concerns, email contact@skymu.app.
+/*==========================================================*/
+// Modification or redistribution of this code is contingent
+// on your agreement to be bound by the terms of our License.
+// If you do not wish to abide by those terms, you may not
+// use, modify, or distribute any code from the Skymu project.
+// License: https://skymu.app/legal/license
+/*==========================================================*/
+
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
