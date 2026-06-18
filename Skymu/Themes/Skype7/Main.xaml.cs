587a5fb7607f2f1a1aec6090921ed9d8ac55f953
OmegaAOL <hackersword666@gmail.com>
2026-06-18 01:51:34 -0700

    Update copyright notice in all files

--- Skymu/Themes/Skype7/Main.xaml.cs
+++ Skymu/Themes/Skype7/Main.xaml.cs
@@ -1,9 +1,9 @@
 /*==========================================================*/
-// Skymu is copyrighted by The Skymu Team, 2026.
+// Copyright © The Skymu Team and other contributors.
 // For any inquiries or concerns, email contact@skymu.app.
 /*==========================================================*/
 // Modification or redistribution of this code is contingent
-// on your agreement to be bound by the terms of our License.
+// on your agreement to be bound by the terms of our license.
 // If you do not wish to abide by those terms, you may not
 // use, modify, or distribute any code from the Skymu project.
 // License: https://skymu.app/legal/license
